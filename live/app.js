5e9e1d046130d6bd912b812ad505c2cff9c616e4
piecioshka <piecioshka@gmail.com>
2012-11-28 23:54:51 +0100

    add create server

--- live/app.js
+++ live/app.js
@@ -1,5 +1,22 @@
 var utils = require("util");
-var io = require('socket.io').listen(4000);
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
+app.listen(80);
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
 
 io.sockets.on('connection', function (client) {
     client.emit("connection");
